3ba4361575bdc4538a2a2892004b96032e834fab
Husniddin2019 <98811010+Husniddin2019@users.noreply.github.com>
2022-02-22 09:35:39 +0500

    Update HomePage.dart

--- HomePage.dart
+++ HomePage.dart
@@ -1,25 +1,75 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:untitled1/DetailsPage.dart';
-class HomePage extends StatefulWidget {
-  static final String id = "HomePage";
-  //const HomePage({Key? key}) : super(key: key);
+class home extends StatefulWidget {
+ // const home({Key? key}) : super(key: key);
+
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _homeState createState() => _homeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _homeState extends State<home> {
+  var  _email;
+  var  _password;
+  final _formkey = GlobalKey<FormState>();
+void _qilgin(){
+  if (_formkey.currentState!.validate()){
+    _formkey.currentState!.save();
+    print("Welcome");
+
+  }
+}
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-            onPressed: (){
-              Navigator.pushNamed(context, DeTailPage.id);
-            },
-            child: Text(
-              "salom"
-            ))
+      //backgroundColor: Colors.grey,
+      // appBar: AppBar(
+      //   title: Text("PDP"),
+      //   centerTitle: true,
+      // ),
+      body: Form(
+        key: _formkey,
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Center(
+                child: Text("Instagram",style: TextStyle(fontSize: 42),),
+              ),
+            ),
+            Padding( padding:EdgeInsets.symmetric(horizontal: 30,vertical: 0) ,
+
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "email"),
+                validator: (input) =>
+                     !input!.contains("@") ? 'pochta tering': null,
+                onSaved: (input) => _email = input,
+              ),
+            ),
+            Padding( padding:EdgeInsets.symmetric(horizontal: 30,vertical: 0) ,
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "password"),
+                validator: (input) =>
+                input!.length < 6 ?  'xato parol': null,
+                onSaved: (input) => _password = input,
+              ),
+            ),
+            FlatButton(padding:EdgeInsets.symmetric(horizontal: 150,vertical: 0), color: Colors.blue, onPressed: (){_qilgin();}, child: Text("Log in",style: TextStyle(color: Colors.white70,fontSize: 24))),
+            Row(
+              children: [
+                FlatButton(padding:EdgeInsets.symmetric(horizontal: 50,vertical: 0), onPressed: (){}, child: Text("Don't have an account?")),
+                FlatButton( padding:EdgeInsets.symmetric(horizontal: 20,vertical: 0),  onPressed: (){}, child: Text("Sign up",style: TextStyle(fontWeight: FontWeight.bold),)),
+              ],
+            )
+          ],
+        ),
+
+
       ),
     );
   }
